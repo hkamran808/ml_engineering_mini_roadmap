3ea3893c3a5c7e22db28759d581632acabbe9ded
Kamran Huseynli <k.huseynli@ufaz.az>
2026-03-12 04:16:19 +0400

    Day2: GridSearch used for finding best models, being expensive due to it exhaustive behaviour. Best model is resaved

--- train_baseline.py
+++ train_baseline.py
@@ -7,14 +7,12 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import LabelEncoder
 
-# 1. BASELINE DATA
 train = pd.read_csv("application_train.csv")
 test = pd.read_csv("application_test.csv")
 
 print("Train shape:", train.shape)
 print("Test shape:", test.shape)
 
-# 2. CLEANING
 train = train.drop(columns=["SK_ID_CURR"])
 test_ids = test["SK_ID_CURR"]
 test = test.drop(columns=["SK_ID_CURR"])
@@ -22,7 +20,6 @@ test = test.drop(columns=["SK_ID_CURR"])
 y = train["TARGET"]
 X = train.drop(columns=["TARGET"])
 
-# 3. HANDLING CATEGORICAL FEATURES
 cat_cols = X.select_dtypes(include=["object"]).columns
 for col in cat_cols:
     
@@ -34,20 +31,15 @@ for col in cat_cols:
     X[col] = le.transform(X[col].astype(str))
     test[col] = le.transform(test[col].astype(str))
 
-
-# 4. SIMPLE FEATURE ENGINEERING
 X["CREDIT_INCOME_RATIO"] = X["AMT_CREDIT"] / (X["AMT_INCOME_TOTAL"] + 1)
 X["ANNUITY_INCOME_RATIO"] = X["AMT_ANNUITY"] / (X["AMT_INCOME_TOTAL"] + 1)
 
 test["CREDIT_INCOME_RATIO"] = test["AMT_CREDIT"] / (test["AMT_INCOME_TOTAL"] + 1)
 test["ANNUITY_INCOME_RATIO"] = test["AMT_ANNUITY"] / (test["AMT_INCOME_TOTAL"] + 1)
 
-# 5. CLEANING FEATURE NAMES
 X.columns = X.columns.str.replace('[^A-Za-z0-9_]+', '', regex=True)
 test.columns = test.columns.str.replace('[^A-Za-z0-9_]+', '', regex=True)
 
-
-# 6. LIGHTGBM PARAMETERS
 lgb_params = {
 
     "n_estimators": 10000,
@@ -60,15 +52,36 @@ lgb_params = {
     "random_state": 42,
     "n_jobs": -1}
 
+# day2
+from sklearn.model_selection import GridSearchCV
+param_grid = {
+    "num_leaves": [31, 64, 128],
+    "max_depth": [-1, 8, 12],
+    "learning_rate": [0.01, 0.05, 0.1],
+    "min_child_samples": [20, 50],
+    "feature_fraction": [0.7, 0.8, 0.9]}
 
-# 7. CROSS VALIDATION
-skfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+base_model = lgb.LGBMClassifier(n_estimators=2000, random_state=1, n_jobs=-1)
+grid_search = GridSearchCV(estimator=base_model, 
+                           param_grid=param_grid, 
+                           cv=3, 
+                           scoring="roc_auc", 
+                           verbose=2, 
+                           n_jobs=-1)
+grid_search.fit(X, y)
+print(10*"-", "best parameters\n", 10*"-")
+print(grid_search.best_params_)
+print(10*"-", "best roc-auc\n", 10*"-")
+print(grid_search.best_score_)
+
+best_model = grid_search.best_estimator_
+joblib.dump(best_model, "best_lgbm_model_fromDAY2.pkl")
+
+"""
+skfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=1)
 
 oof_preds = np.zeros(len(X))
 feature_importances = np.zeros(X.shape[1])
-
-
-# 8. TRAINING LOOP TO GET OOF PREDICTIONS AND FEATURE IMPORTANCES
 
 for fold, (train_idx, val_idx) in enumerate(skfold.split(X, y)):
 
@@ -96,20 +109,15 @@ for fold, (train_idx, val_idx) in enumerate(skfold.split(X, y)):
 
     feature_importances += model.feature_importances_ / skfold.n_splits
 
-
-# 9. FINAL METRICS
 full_auc = roc_auc_score(y, oof_preds)
 print("\nOverall OOF ROC AUC:", full_auc)
 
-# 10. TRAIN FINAL MODEL
 final_model = lgb.LGBMClassifier(**lgb_params)
 final_model.fit(X, y)
 
-# 11. MODEL SAVING
 joblib.dump(final_model, "credit_risk_lgbm.pkl")
 print("Model saved.")
 
-# 12. TEST PREDICTIONS
 test_preds = final_model.predict_proba(test)[:, 1]
 
 # day1
@@ -121,3 +129,4 @@ importance_df = pd.DataFrame({
 }).sort_values("importance", ascending=False)
 
 print(importance_df.head(20))
+"""
